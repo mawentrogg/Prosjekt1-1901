6b0972e091d5d22dfd6163cb38b2557fdf569b3d
Kim Erling Rasmussen <kimmern92@gmail.com>
2017-09-07 10:47:39 +0200

    added test class, seems to be working, it's beeen some time since I did this java stuff

---
+++ src/Core/Main.java
@@ -0,0 +1,12 @@
+package Core;
+
+/**
+ * Created by kimme on 07.09.2017.
+ */
+
+public class Main {
+    public static void main(String args[]) {
+        Test test = new Test();
+        test.hurray();
+    }
+}
--- src/Main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by kimme on 07.09.2017.
- */
-public class Main {
-}
