6582b3469a92a1e0fe781df7f4760fa9fedc1677
unknown <jonasjaas@gmail.com>
2017-09-25 10:22:35 +0200

    endringer lokalt

--- src/organizer.php
+++ src/organizer.php
@@ -5,23 +5,22 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body style="background-color: #3C6E71">
-
 	<div class="flexBody">
 		<div class="flexWrapper">
-			<form action="includes\logout.inc.php" method="post">
-			<button type="submit" name="submit">Logg ut</button>
-			</form>
-				<div class="flexWrapperInside">
-					<table>
-						<tr>
-							<th style="color: white; background-color: #353535;">Oversikt</th>
-						</tr>
-						<tr>
-							<td><a href="rigge-oversikt.php">Rigge-oversikt</a></td>
-						</tr>
-					</table>
+			<div class="flexWrapperTop">
+				<p class="menuHeader">Arrangør//Admin</p>
+				<form action="includes\logout.inc.php" method="post">
+				<button type="submit" name="submit">Logg ut</button>
+				</form>
 				</div>
+			<div class="flexWrapperInside">
+				<table>
+					<tr>
+						<td><a href="rigge-oversikt.php">Rigge-oversikt</a></td>
+					</tr>
+				</table>
 			</div>
 		</div>
-	</body>
+	</div>
+</body>
 </html>
