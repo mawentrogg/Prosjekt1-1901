e204d24f16c861955d67082367480985ccde8c67
havardhuns <havardhuns@gmail.com>
2017-10-02 12:00:44 +0200

    henter url-variabel fra databasen til booking offers

--- src/booking-reply.php
+++ src/booking-reply.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+include_once 'includes/dbh.inc.php';
+
+$band = $_GET["band"]
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,10 +20,24 @@
             <p class="insideMenuHeader">Your booking offer</p>
             <div class="flexWrapperInside" style="background-color: #353535">
 
+              <?php
+              $sql = "SELECT * FROM Booking_Offers WHERE bandName = '$band'";
+              $result = mysqli_query($conn, $sql);
+              $offer_result = $result->fetch_all();
+              print_r($offer_result);
+              if ($offer_result[0][6] == 0) {
+                echo "ikke accepted";
+                //godta/ikke-godta
+                //legg til band
+                //legg til konsert
+              } else {
+                echo "accepted";
+                //header("Location: add-demands.php");
+                //$concertId = hent concert id
+              }
+              ?>
 
 
-                
-                            
 
 
 
@@ -21,7 +45,7 @@
 
             </div>
 
-            
+
         </div>
 
     </table>
--- src/send-booking-mail.php
+++ src/send-booking-mail.php
@@ -16,29 +16,29 @@ $email = $_POST['email'];
 $startTime = date($date . ' ' . $time);
 
 
-
 $endTime = date($date . ' ' . $time); //pluss $length minutter
 
-$sql = "INSERT INTO Booking_Offers (BandName, ConcertTimeStart, ConcertTimeEnd, SceneID, ContactEmail)
-VALUES ('$bandName', '$startTime', '$endTime', $scene, '$email')";
+
+$sql = "INSERT INTO Booking_Offers (BandName, ConcertTimeStart, ConcertTimeEnd, SceneID, ContactEmail, Accepted)
+VALUES ('$bandName', '$startTime', '$endTime', $scene, '$email', 0)";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    mail($email, utf8_decode("Booking offer for ") . utf8_decode($bandName),
+	"Your band " . $bandName . " has received an offer to play at festiv4len on " . $date . ", " . $time . " on Stage " . $scene .
+	". Your set will last " . $length . " minutes\n\n" .
+	"For the concert, you will be paid " . $price . "kr \n\nClick the following link to review your offer. \n\n\n"
+	. "http://org.ntnu.no/festiv4len/booking-reply.php?band=" . $bandName);
+
+	$_SESSION['sent'] = true;
+	$_SESSION['mail'] = $email;
+	$_SESSION['band'] = $bandName;
+
+  header("Location: booking-offer.php");
+
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 
-mail($email, utf8_decode("Booking offer for ") . utf8_decode($bandName),
-"Your band " . $bandName . " has received an offer to play at festiv4len on " . $date . ", " . $time . " on Stage " . $scene .
-". Your set will last " . $length . " minutes\n\n" .
-"For the concert, you will be paid " . $price . "kr \n\nClick the following link to review your offer. \n\n\n"
-. "http://org.ntnu.no/festiv4len/booking-reply.php?band=" . $bandName);
-
-$_SESSION['sent'] = true;
-$_SESSION['mail'] = $email;
-$_SESSION['band'] = $bandName;
-
-header("Location: booking-offer.php");
 
 ?>
