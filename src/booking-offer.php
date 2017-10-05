e1575f7c5ef197e8acaf827f7bc59853b709e822
havardhuns <havardhuns@gmail.com>
2017-10-05 15:35:37 +0200

    skal funke naa

--- src/booking-offer.php
+++ src/booking-offer.php
@@ -23,20 +23,6 @@ $username = $_SESSION['u_username'];
 </head>
 <body style="background-color: #3C6E71">
     <div class="flexBody">
-    <div class="flexTop">
-        <a class="hjemButton" href="<?php
-                    if(isset($_SESSION['u_id'])){
-                        echo $_SESSION['u_role'] . ".php";
-                    }
-                    else{
-                        echo "index.html";
-                    }
-                    ?>">Hjem</a>
-        <p class="superHeader">Festiv4len</p>
-        <form action="includes\logout.inc.php" method="post">
-            <button type="submit" name="submit">Logg ut</button>
-        </form> 
-    </div>
         <div style="width:auto;height:70vh;" class="flexWrapper">
             <p class="insideMenuHeader">Send booking offer</p>
             <div class="flexWrapperInside" style="background-color: #353535">
@@ -67,8 +53,30 @@ $username = $_SESSION['u_username'];
                       session_destroy();
                     }
                 ?>
+                            
+
+
+
+
+
             </div>
+
+            <a class="hjemButton" href="<?php
+                    if(isset($_SESSION['u_id'])){
+                        echo $_SESSION['u_role'] . ".php";
+                    }
+                    else{
+                        echo "index.html";
+                    }
+                    ?>">Hjem</a>
+
+            <form action="includes\logout.inc.php" method="post">
+                <button type="submit" name="submit">Logg ut</button>
+            </form> 
         </div>
+
+    </table>
+
     </div>
 </body>
 </html>
--- src/booking-reply.php
+++ src/booking-reply.php
@@ -13,23 +13,22 @@ $band = $_GET["band"]
 <head>
     <title>Reply to booking offer</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script>
+      function confirmDelete() {
+        return confirm("Are you sure you want to decline the offer?");
+      }
+
+
+
+
+
+
+
+
+    </script>
 </head>
 <body style="background-color: #3C6E71">
     <div class="flexBody">
-     <div class="flexTop">
-        <a class="hjemButton" href="<?php
-                    if(isset($_SESSION['u_id'])){
-                        echo $_SESSION['u_role'] . ".php";
-                    }
-                    else{
-                        echo "index.html";
-                    }
-                    ?>">Hjem</a>
-        <p class="superHeader">Festiv4len</p>
-        <form action="includes\logout.inc.php" method="post">
-            <button type="submit" name="submit">Logg ut</button>
-        </form> 
-    </div>
         <div style="width:auto;height:70vh;" class="flexWrapper">
             <p class="insideMenuHeader">Your booking offer</p>
             <div class="flexWrapperInside" style="background-color: #353535">
@@ -38,22 +37,146 @@ $band = $_GET["band"]
               $sql = "SELECT * FROM Booking_Offers WHERE bandName = '$band'";
               $result = mysqli_query($conn, $sql);
               $offer_result = $result->fetch_all();
-              if ($offer_result[0][6] == 0) {
-                <table>
-                  <tr>
-                    <th>
-                  </tr>
-                </table>
-                echo "ikke accepted";
-                echo '<input type="submit" value="Accept">
-                <input type="submit" value="Decline">';
-                //legg til band
-                //legg til konsert
+
+              $bandName = $offer_result[0][1];
+
+
+              if($band == $bandName)
+              {
+
+
+
+
+
+                $concertStart = $offer_result[0][2];
+                $concertEnd = $offer_result[0][3];
+                $scene = $offer_result[0][4];
+                $email = $offer_result[0][5];
+                $id = $offer_result[0][0];
+
+
+
+
+
+                if ($offer_result[0][6] == 0) {
+
+
+                  echo "<table>
+                    <tr>
+                      <th>Band</th>
+                      <th>Concert start time</th>
+                      <th> Concert end time </th>
+                      <th> Scene </th>
+                    </tr>
+                    <tr>
+                      <th>" . $bandName . " </th>
+                      <th>" . $concertStart . " </th>
+                      <th>" . $concertEnd . " </th>
+                      <th>" . $scene . " </th>
+                    </tr>
+
+                  </table>";
+
+
+
+                  echo '<form method="post"> <input type="submit" value="Accept" name="accept"></form>
+                  <form method="post" onsubmit="return confirmDelete();"> <input type="submit" value="Decline" name="decline" ></form>';
+
+                  if(isset($_POST['accept'])){
+                    $updateAccept = "UPDATE Booking_Offers SET Accepted=1 WHERE BookingOfferID=" . $id;
+                    if ($conn->query($updateAccept) === TRUE) {
+                        echo "Record updated successfully";
+                        $sql2 = "INSERT IGNORE INTO Band (BandName, Manager)
+                        VALUES ('$bandName', '$email')";
+
+                        if ($conn->query($sql2) === TRUE) {
+                          $sql3 = "SELECT * FROM Band WHERE bandName = '$band'";
+                          $result2 = mysqli_query($conn, $sql3);
+                          $offer_result2 = $result2->fetch_all();
+                          $BandID = $offer_result2[0][0];
+
+                          $sql4 = "INSERT IGNORE INTO Concert (ConcertTimeStart, ConcertTimeEnd, SceneID, BandID)
+                          VALUES ('$concertStart', '$concertEnd', $scene, $BandID)";
+
+                          if ($conn->query($sql4) === TRUE) {
+                            $concert = "SELECT * FROM Concert WHERE BandID =" . $BandID;
+                            $result3 = mysqli_query($conn, $concert);
+                            $concertResult = $result3->fetch_all();
+
+                            $_SESSION['concertID'] = $concertResult[0][0];
+
+                            $concertID2 = $concertResult[0][0];
+                            echo '<p>The Offer has been accepted, you will be redirected to Add Demands in <span id="counter">3</span> second(s).<br><br></p>';
+
+
+                            echo "Your concert ID: " . $concertID2;
+
+                            header('refresh: 3; url=add-demands.php');
+
+
+
+                          } else {
+                              echo "Error: " . $sql4 . "<br>" . $conn->error;
+                          }
+                        } else {
+                            echo "Error: " . $sql2 . "<br>" . $conn->error;
+                        }
+                    } else {
+                        echo "Error updating record: " . $conn->error;
+                    }
+
+
+
+
+
+
+                  }
+                  if(isset($_POST['decline'])){
+                    $delete = "DELETE FROM Booking_Offers WHERE BandName= '$band'";
+
+                    if ($conn->query($delete) === TRUE) {
+                        echo "Offer declined";
+                    } else {
+                        echo "Error deleting offer: " . $conn->error;
+                    }
+                  }
+
+
+
+
+
+
+                } else {
+
+                  $bands = "SELECT * FROM Band WHERE bandName = '$band'";
+                  $result2 = mysqli_query($conn, $bands);
+                  $offer_result2 = $result2->fetch_all();
+                  $BandID = $offer_result2[0][0];
+
+                  $concert = "SELECT * FROM Concert WHERE BandID = " . $BandID;
+                  $result3 = mysqli_query($conn, $concert);
+                  $concertResult = $result3->fetch_all();
+
+
+
+
+
+                  $_SESSION['concertID'] = $concertResult[0][0];
+                  $concertID2 = $concertResult[0][0];
+                  echo '<p>You have already accepted the offer, and will be redirected to Add Demands in <span id="counter">5</span> second(s).<br><br></p>';
+
+
+                  echo "Your concert ID: " . $concertID2;
+
+                  header('refresh: 5; url=add-demands.php');
+
+                }
               } else {
-                echo "accepted";
-                //header("Location: add-demands.php");
-                //$concertId = hent concert id
+                echo "Your band " . $band . "does not have an offer";
               }
+
+
+
               ?>
 
 
